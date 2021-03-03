f8def0615bc0787b3c12aa7aec79ecf55822e53b
taemojin <taemojin@nate.com>
2021-03-03 16:16:16 +0900

    pom.xml 수정

--- src/test/java/kr/co/study/Test.java
+++ src/test/java/kr/co/study/Test.java
@@ -6,10 +6,11 @@ import javax.inject.Inject;
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactory; 
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations ={"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
